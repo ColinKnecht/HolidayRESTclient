45e48603858fba6bb7f056c64630abdcaf6694e9
ColinKnecht <cknecht87@gmail.com>
2017-10-12 10:53:31 -0400

    First Commit

---
+++ src/main/RESTMain.java
@@ -0,0 +1,22 @@
+package main;
+
+/**
+ * Calls the RESTful web service at REST_URL defined below
+ * @author colinknecht
+ *
+ */
+public class RESTMain {
+	public static final String REST_URL = "www.url.com";
+
+	/**
+	 * Call the web service and display the response
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// call the service
+		
+		//process the response object
+
+	}
+
+}
