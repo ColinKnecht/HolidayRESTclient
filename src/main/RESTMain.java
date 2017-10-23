6bfa80190719e3d5d79fe834296d077a42fcfe20
ColinKnecht <cknecht87@gmail.com>
2017-10-23 10:44:20 -0400

    Initial Commit

--- src/main/RESTMain.java
+++ src/main/RESTMain.java
@@ -1,5 +1,9 @@
 package main;
 
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 /**
  * Calls the RESTful web service at REST_URL defined below
  * @author colinknecht
@@ -7,13 +11,20 @@ package main;
  */
 public class RESTMain {
 	public static final String REST_URL = "www.url.com";
+	
+	public static final int OK_STATUS = Response.Status.OK.getStatusCode();
 
 	/**
 	 * Call the web service and display the response
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// call the service
+		// call the service and get the response object
+		Response response = ClientBuilder.newClient()
+				.target(REST_URL)
+				.request(MediaType.APPLICATION_XML)
+				.get();
+				
 		
 		//process the response object
 
